1c191a819fdcfeadfe51fde12b82f4e7905a7b3f
Alvin Yanson <yanson.alvin.info@gmail.com>
2023-07-14 20:53:04 +0800

    done ng-sc

--- Script.js
+++ Script.js
@@ -3,11 +3,11 @@
 var myApp = angular.module("myModule", []);
 
 myApp.controller("myController", function ($scope) {
-    var employee = {
-        firstName: 'David',
-        lastName: 'Hastings',
-        gender: 'Male'
-    };
-    
-    $scope.employee = employee
+    var country = {
+        name: 'PAGCOR',
+        capital: '3 Million Logo 😭',
+        flag: "images/pagcor.png"
+    }
+
+    $scope.country = country;
 });
