6f3cba2b1cc2b050b911966fe079402122093341
Alvin Yanson <yanson.alvin.info@gmail.com>
2023-07-15 12:03:38 +0800

    done

--- Script.js
+++ Script.js
@@ -3,21 +3,17 @@
 var myApp = angular.module("myModule", []);
 
 myApp.controller("myController", function ($scope) {
+    // filter can do 3 things, format, sort, and filter
 
-    var technologies = [
-        { name: "C#", likes: 0, dislikes: 0 },
-        { name: "ASP.NET", likes: 0, dislikes: 0 },
-        { name: "SQL", likes: 0, dislikes: 0 },
-        { name: "AngularJS", likes: 0, dislikes: 0 },
+
+    var employees = [
+        { name: "Ben", dateOfBirth: new Date("November 23, 1980"), gender: "Male", salary: 55000.788 },
+        { name: "Sara", dateOfBirth: new Date("May 05, 1970"), gender: "Female", salary: 68000 },
+        { name: "Pam", dateOfBirth: new Date("October 27, 1979"), gender: "Female", salary: 53000 },
+        { name: "Todd", dateOfBirth: new Date("December 30, 1983"), gender: "Male", salary: 60000 },
     ]
 
-    $scope.technologies = technologies;
+    $scope.employees = employees;
+    $scope.rowLimit = 3;
 
-    $scope.incrementLikes = function (technology) {
-        technology.likes++;
-    }
-
-    $scope.incrementDislikes = function (technology) {
-        technology.dislikes++;
-    }
 });
