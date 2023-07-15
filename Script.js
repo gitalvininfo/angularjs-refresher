7b932c02959bdebae2383297f694433353b6fd39
Alvin Yanson <yanson.alvin.info@gmail.com>
2023-07-15 13:50:02 +0800

    done

--- Script.js
+++ Script.js
@@ -4,13 +4,13 @@ var app = angular.module("myModule", []);
 
 app.controller("myController", function ($scope) {
     var employees = [
-        { name: "Ben", gender: 1, salary: 55000.788 },
-        { name: "Sara", gender: 2, salary: 68000 },
-        { name: "Pam", gender: 2, salary: 53000 },
-        { name: "Todd", gender: 1, salary: 60000 },
-        { name: "Finn", gender: 3, salary: 60000 },
+        { city: "London", name: "Ben", gender: 1, salary: 55000.788 },
+        { city: "London", name: "Sara", gender: 2, salary: 68000 },
+        { city: "London", name: "Pam", gender: 2, salary: 53000 },
+        { city: "London", name: "Todd", gender: 1, salary: 60000 },
+        { city: "London", name: "Finn", gender: 3, salary: 60000 },
     ]
 
     $scope.employees = employees;
-
+    
 });
