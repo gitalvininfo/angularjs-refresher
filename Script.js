4a36fa07d25e008397fc5d6c47f3d12796b75fc9
Alvin Yanson <yanson.alvin.info@gmail.com>
2023-07-15 13:07:13 +0800

    done

--- Script.js
+++ Script.js
@@ -3,37 +3,13 @@
 var myApp = angular.module("myModule", []);
 
 myApp.controller("myController", function ($scope) {
-    // | orderBy : expression : reverse
-    // to sort in asc order, set reverse to false
-    // to sort in desc order, set reverse to true
     var employees = [
-        { name: "Ben", dateOfBirth: new Date("November 23, 1980"), gender: "Male", salary: 55000.788 },
-        { name: "Sara", dateOfBirth: new Date("May 05, 1970"), gender: "Female", salary: 68000 },
-        { name: "Pam", dateOfBirth: new Date("October 27, 1979"), gender: "Female", salary: 53000 },
-        { name: "Todd", dateOfBirth: new Date("December 30, 1983"), gender: "Male", salary: 60000 },
-    ]
-
-    var sortOptions = [
-        { value: "name", label: "Name ASC" },
-        { value: "gender", label: "Gender ASC" },
-        { value: "dateOfBirth", label: "Date of Birth ASC" },
-        { value: "-salary", label: "Salary DESC" },
+        { city: "London", name: "Ben", gender: "Male", salary: 55000.788 },
+        { city: "Los Angeles", name: "Sara", gender: "Female", salary: 68000 },
+        { city: "Tokyo", name: "Pam", gender: "Female", salary: 53000 },
+        { city: "Madrid", name: "Todd", gender: "Male", salary: 60000 },
     ]
 
     $scope.employees = employees;
-    $scope.sortColumn = "name";
-    $scope.sortOptions = sortOptions;
-    $scope.reverseSort = false;
 
-    $scope.sortData = function (column) {
-        $scope.reverseSort = ($scope.sortColumn == column) ? !$scope.reverseSort : false
-        $scope.sortColumn = column;  
-    }
-
-    $scope.getSortClass = function (column) {
-        if($scope.sortColumn == column) {
-            return $scope.reverseSort ? 'arrow-down' : 'arrow-up'
-        }
-        return '';
-    }
 });
