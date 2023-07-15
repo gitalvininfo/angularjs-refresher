0333ad0f2f25aea14d54a53ef92d12f33d3da9c1
Alvin Yanson <yanson.alvin.info@gmail.com>
2023-07-15 13:37:13 +0800

    done

---
+++ Filters.js
@@ -0,0 +1,11 @@
+/// <reference path="Script.js" />
+
+app.filter("gender", function () {
+    return function (gender) {
+        switch (gender) {
+            case 1: return "Male";
+            case 2: return "Female";
+            default: return "Not disclosed";
+        }
+    }
+})
--- Script.js
+++ Script.js
@@ -1,28 +1,16 @@
 /// <reference path="angular.js" />
 
-var myApp = angular.module("myModule", []);
+var app = angular.module("myModule", []);
 
-myApp.controller("myController", function ($scope) {
+app.controller("myController", function ($scope) {
     var employees = [
-        { city: "London", name: "Ben", gender: "Male", salary: 55000.788 },
-        { city: "Los Angeles", name: "Sara", gender: "Female", salary: 68000 },
-        { city: "Tokyo", name: "Pam", gender: "Female", salary: 53000 },
-        { city: "Madrid", name: "Todd", gender: "Male", salary: 60000 },
-        { city: "Moscow", name: "Finn", gender: "Male", salary: 60000 },
+        { name: "Ben", gender: 1, salary: 55000.788 },
+        { name: "Sara", gender: 2, salary: 68000 },
+        { name: "Pam", gender: 2, salary: 53000 },
+        { name: "Todd", gender: 1, salary: 60000 },
+        { name: "Finn", gender: 3, salary: 60000 },
     ]
 
     $scope.employees = employees;
-
-    $scope.search = function (item) {
-        if ($scope.searchText == undefined) {
-            return true;
-        } else {
-            if (item.name.toLowerCase().indexOf($scope.searchText.toLowerCase()) != -1 ||
-                item.city.toLowerCase().indexOf($scope.searchText.toLowerCase()) != -1) {
-                return true;
-            }
-        }
-        return false;
-    }
 
 });
