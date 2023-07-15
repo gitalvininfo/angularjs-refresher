6db7c8d81f17461217707e0a0f4c7b5923008a0b
Alvin Yanson <yanson.alvin.info@gmail.com>
2023-07-15 14:45:15 +0800

    done

--- Script.js
+++ Script.js
@@ -2,16 +2,10 @@
 
 var app = angular.module("myModule", []);
 
-app.controller("myController", function ($scope) {
-    var employees = [
-        { name: "Ben", gender: "Male", salary: 55000.788 },
-        { name: "Sara", gender: "Female", salary: 68000 },
-        { name: "Pam", gender: "Female", salary: 53000 },
-        { name: "Todd", gender: "Male", salary: 60000 },
-        { name: "Finn", gender: "Male", salary: 60000 },
-    ]
+app.controller("myController", function ($scope, $http) {
 
-    $scope.employees = employees;
-    $scope.employeeView = 'EmployeeTable.html';
-
+    $http.get("https://jsonplaceholder.typicode.com/todos/")
+        .then(function (response) {
+            $scope.todos = response.data
+        });
 });
